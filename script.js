356d73c2be29f84828ee6742a885a9d5b2aab205
N-Endy <okafornel@gmail.com>
2021-07-07 14:33:06 +0100

    refractor code

--- script.js
+++ script.js
@@ -5,14 +5,10 @@ let btns = document.querySelectorAll('a');
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        if (btn.className === 'lower') {
-            number.textContent--;
-        } else {
-            number.textContent++;
-        }
+        btn.className === 'lower' ? number.textContent-- : number.textContent++;
 
         number.innerText > 0 ? number.style.color = 'green' : number.style.color = 'black';
         number.innerText == 0 ? lower.classList.add('disabled') : lower.classList.remove('disabled');
         number.innerText == 50 ? add.classList.add('disabled') : add.classList.remove('disabled');
-    })
-})
+    });
+});
